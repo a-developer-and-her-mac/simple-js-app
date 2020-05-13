aedf2e95992c58c339aa78255eb4998bb888d4e8
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-05-13 08:03:20 -0700

    Updated script.js file to contain a new array with objects
    Deleted original contents and replaced them with an array containing objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello World!');
-let favoriteFood="icecream";
-document.write(favoriteFood);
+let pokemonList=[
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['Ground', 'Rock', 'Water', 'Steel', 'Fire', 'Ice', 'Fairy']
+  }
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['Grass', 'Electric', 'Steel', 'Fire', 'Water', 'Ice']
+    }
+    {
+      name: 'Fearow',
+      height: 1.2,
+      types: ['Rock', 'Electric', 'Ice', 'Bug', 'Grass', 'Ghost', 'Ground']
+    }
+];

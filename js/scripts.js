8fb4537c747d5f7286fccc0300babc6a62fa6fee
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-05-14 07:37:33 -0700

    Added missing commas in object

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ let pokemonList=[
     name: 'Charmander',
     height: 0.6,
     types: ['Ground', 'Rock', 'Water', 'Steel', 'Fire', 'Ice', 'Fairy']
-  }
+  },
   {
     name: 'Squirtle',
     height: 0.5,
     types: ['Grass', 'Electric', 'Steel', 'Fire', 'Water', 'Ice']
-    }
+  },
     {
       name: 'Fearow',
       height: 1.2,

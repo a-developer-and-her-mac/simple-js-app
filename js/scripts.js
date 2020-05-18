b5f9974a17981c76d29da41cca838c0c4e2570ec
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-05-18 07:50:43 -0700

    Added styles to .css file, added for loop to .js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-let pokemonList=[
-  {
+let pokemonList = [{
     name: 'Charmander',
     height: 0.6,
     types: ['Ground', 'Rock', 'Water', 'Steel', 'Fire', 'Ice', 'Fairy']
@@ -9,9 +8,16 @@ let pokemonList=[
     height: 0.5,
     types: ['Grass', 'Electric', 'Steel', 'Fire', 'Water', 'Ice']
   },
-    {
-      name: 'Fearow',
-      height: 1.2,
-      types: ['Rock', 'Electric', 'Ice', 'Bug', 'Grass', 'Ghost', 'Ground']
-    }
+  {
+    name: 'Fearow',
+    height: 1.2,
+    types: ['Rock', 'Electric', 'Ice', 'Bug', 'Grass', 'Ghost', 'Ground']
+  }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<h2>' + pokemonList[i].name + '</h2>' + 'Height: ' + pokemonList[i].height + ' ');
+  if (pokemonList[i].height >= 1) {
+    document.write('\(Wow, that\'s big!\)');
+  }
+}

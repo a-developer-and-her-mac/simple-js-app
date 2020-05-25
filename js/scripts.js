44e96aeab2f941100c04d82b81f6ef1ae0268b93
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-05-25 07:58:09 -0700

    Updated script.js file
    Wrapped the pokemonList array in a IIFE, assigned it to a variable, then updated the forEach loop, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-let pokemonList = [{
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Ground', 'Rock', 'Water', 'Steel', 'Fire', 'Ice', 'Fairy']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['Grass', 'Electric', 'Steel', 'Fire', 'Water', 'Ice']
-  },
-  {
-    name: 'Fearow',
-    height: 1.2,
-    types: ['Rock', 'Electric', 'Ice', 'Bug', 'Grass', 'Ghost', 'Ground']
-  }
-];
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Charmander',
+      height: 0.6,
+      types: ['Ground', 'Rock', 'Water', 'Steel', 'Fire', 'Ice', 'Fairy']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['Grass', 'Electric', 'Steel', 'Fire', 'Water', 'Ice']
+    },
+    {
+      name: 'Fearow',
+      height: 1.2,
+      types: ['Rock', 'Electric', 'Ice', 'Bug', 'Grass', 'Ghost', 'Ground']
+    }
+  ];
 
-pokemonList.forEach(function(pokemonList) {
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemonList) {
   document.write('<h2>' + pokemonList.name + '</h2>' + 'Height: ' + pokemonList.height + ' ');
 
 });
